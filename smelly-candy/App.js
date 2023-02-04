c9848f9d44292dddd1ae03c0f07000cf46d2dc77
Swayam Soni <80837512+string-SAm@users.noreply.github.com>
2023-02-05 00:13:38 +0530

    Add files via upload

---
+++ smelly-candy/App.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from './src/screen/Home';
+import About from './src/screen/About';
+import Course from "./src/screen/Course";
+import UserData from "./src/screen/UserData";
+import CourseDetails from "./src/screen/Coursedetails";
+import Contact from "./src/screen/Contact";
+
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        {/* home screen  */}
+        <Stack.Screen
+          name="Home"
+          options={{
+            headerShown: false,
+          }}>
+          {(props) => <Home {...props} channelName={"Thapa Technical"} />}
+        </Stack.Screen>
+
+        {/* Course Screen  */}
+        <Stack.Screen
+          name="Course"
+          component={Course}
+          options={{
+            headerTitleStyle: {
+              fontSize: 25,
+              fontFamily: "Nunito_700Bold",
+            },
+            headerTitle: "Courses",
+            headerTitleAlign: "center",
+          }}
+        />
+
+        {/* UserData Screen  */}
+        <Stack.Screen
+          name="Student"
+          component={UserData}
+          options={{
+            headerTitleStyle: {
+              fontSize: 25,
+              fontFamily: "Nunito_700Bold",
+            },
+            headerTitle: "Students Data",
+            headerTitleAlign: "center",
+          }}
+        />
+
+        {/* About Screen  */}
+        <Stack.Screen
+          name="About"
+          component={About}
+          options={{
+            headerTitleStyle: {
+              fontSize: 25,
+              fontFamily: "Nunito_700Bold",
+            },
+            headerTitleAlign: "center",
+          }}
+        />
+
+        {/* Contact Screen  */}
+        <Stack.Screen
+          name="Contact"
+          component={Contact}
+          options={{
+            headerTitleStyle: {
+              fontSize: 25,
+              fontFamily: "Nunito_700Bold",
+            },
+            headerTitleAlign: "center",
+          }}
+        />
+
+        {/* CourseDetails Screen  */}
+        <Stack.Screen
+          name="CourseDetails"
+          component={CourseDetails}
+          options={{
+            headerTitleStyle: {
+              fontSize: 25,
+              fontFamily: "Nunito_700Bold",
+            },
+            headerTitleAlign: "center",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ smelly-candy/src/screen/Home.js
@@ -0,0 +1,103 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import Menu from '../components/Menu';
+const Home = (props) => {
+  
+  const description =
+    "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. ";
+
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.homeTop}>
+        <Image
+          style={styles.headerImage}
+          resizeMode="contain"
+          source={{
+            uri: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          }}
+        />
+
+        <Text style={styles.mainHeader}>Welcome to</Text>
+        <Text
+          style={[
+            styles.mainHeader,
+            {
+              fontSize: 33,
+              color: "#4c5dab",
+              marginTop: 0,
+            },
+          ]}>
+          Educational Site
+        </Text>
+
+        <Text style={styles.paraStyle}>{description} </Text>
+      </View>
+
+      <View style={styles.menuStyle}>
+        <View style={styles.lineStyle}></View>
+        <Menu />
+        <View
+          style={[
+            styles.lineStyle,
+            {
+              marginVertical: 10,
+            },
+          ]}></View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    textAlign: "center",
+  },
+
+  homeTop: {
+    // height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+
+  headerImage: {
+    height: undefined,
+    width: "100%",
+    aspectRatio: 1,
+    display: "flex",
+    alignItems: "stretch",
+    marginTop: 50,
+    borderRadius: 20,
+  },
+
+  mainHeader: {
+    fontSize: 30,
+    color: "#344055",
+    textTransform: "uppercase",
+    fontFamily: "Nunito_700Bold",
+  },
+
+  paraStyle: {
+    textAlign: "left",
+    fontSize: 18,
+    color: "#7d7d7d",
+    marginTop: 30,
+    paddingBottom: 50,
+    lineHeight: 27,
+    fontFamily: "WorkSans_400Regular",
+  },
+
+  lineStyle: {
+    marginBottom: 10,
+    borderWidth: 0.5,
+    borderColor: "grey",
+  },
+});
+
+export default Home;
